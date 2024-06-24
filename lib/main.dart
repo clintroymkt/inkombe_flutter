2c250e5a70387ccdaa8d88ce599d55913dad8f77
Clint Mukarakate <clintroymkt@gmail.com>
2024-06-25 00:49:55 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:inkombe_flutter/home.dart';
+
+List<CameraDescription>? cameras;
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Inkombe',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:Home(),
+    );
+
+  }
+}
+
+
