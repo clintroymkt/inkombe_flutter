c5a1daf16f248c385333cecdcc9934054eaee638
willhitman <giftwt9wt@gmail.com>
2024-08-20 11:40:27 +0200

    ... adding databse class for all databse operations

---
+++ lib/services/database_service.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+import 'dart:ffi';
+import 'dart:math';
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class DatabaseService{
+
+  final String? uid;
+  DatabaseService({this.uid});
+
+  final CollectionReference userCollection =
+  FirebaseFirestore.instance.collection("User");
+
+  final CollectionReference cattleCollection =
+  FirebaseFirestore.instance.collection("Cattle");
+
+  Future savingUserData(
+      String email,
+      ) async {
+    return await userCollection.doc(uid).set({
+      "fullName": "",
+      "description": "",
+      "email": email,
+      "cattle": [],
+      "profilePic": "",
+      "uid": uid,
+      'reg': false, //whether account is verified or not
+      'admin' : false //super user access
+    });
+  }
+
+  Future gettingUserData(String email) async {
+    QuerySnapshot snapshot =
+    await userCollection.where("email", isEqualTo: email).get();
+    return snapshot;
+  }
+
+//   create cattle
+Future createCattle(
+    String userDocId,
+    Int age,
+    String breed,
+    String diet,
+    String diseasesAilments,
+    Float height,
+    String location,
+    String modelId,
+    String name,
+    String owner,
+    String weight)
+async {
+  cattleCollection.add({
+    "age": age,
+    "breed":breed,
+    "diet": diet,
+    "diseases/ailments": diseasesAilments,
+    "height(m)": height,
+    "location": location,
+    "name":name,
+    "owner":owner,
+    "weight(kg)":weight,
+    "model_id": "",
+    "owner_uid": uid
+  }
+  ).then((docRef)=>{
+      userCollection.doc(userDocId).update(
+        {
+          "cattle":FieldValue.arrayUnion([docRef.id]) //add cattle to user document
+          //don't forget to remove when deleting cattle
+        }
+      )
+
+  });
+}
+  getCattleUpdates() {
+    return cattleCollection.orderBy("date", descending: true).limit(4).snapshots();
+  }
+
+}
