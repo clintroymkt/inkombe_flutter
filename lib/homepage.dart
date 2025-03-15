4015efb148278e0d5d3c2a7f92feb62488d7ae75
Clint <clintroymkt@gmail.com>
2025-03-15 13:56:52 +0200

    ..

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:inkombe_flutter/pages/camera_exp.dart';
 import 'package:inkombe_flutter/pages/database_page.dart';
 import 'package:inkombe_flutter/pages/home_screen/home_page.dart';
 import 'package:inkombe_flutter/pages/new_scan_page.dart';
@@ -20,10 +21,7 @@ class _HomepageState extends State<Homepage> {
   TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
   static const List<Widget> _widgetOptions = <Widget>[
     HomePage(),
-    Text(
-      'Search',
-      style: optionStyle,
-    ),
+    CameraExp(),
     ScanPage(),
     DatabasePage(),
     NewScanPage()
