72ba91816c011e4548de0621cc4d48e9b31f31ba
willhitman <giftwt9wt@gmail.com>
2025-09-05 18:15:55 +0200

    adding updated setting page

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../Authentication/firebase_auth.dart';
+import '../Authentication/pages/login.dart';
+import '../widgets.dart';
+import '../widgets/alert_dialog.dart';
+import '../widgets/settings_option.dart';
+
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
@@ -8,27 +14,119 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  final AuthService _authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: SafeArea(child: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
           child: Column(
               children: [
-                Card(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.account_circle),
-                          Text("Your Account", style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold
-                          ),)
-                        ],
-                      )
-                    ],
-                  ),
+                Column(
+                  children: [
+                    const Row(
+                      children: [
+                        Icon(Icons.account_circle),
+                        Text("Your Account", style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold
+                        ),),
+                      ],
+                    ),
+                    const SizedBox(height: 10,),
+                    // Email
+                    SettingOptions(
+                      text: 'Email',
+                      onPressed: (){},
+
+                    ),
+                    const SizedBox(height: 2,),
+                    SettingOptions(
+                      text: 'Username',
+                      onPressed: (){},
+
+                    ),
+                    const SizedBox(height: 2,),
+                    SettingOptions(
+                      text: 'Full name',
+                      onPressed: (){},
+
+                    ),
+
+                    SizedBox(height: 30,),
+                    SettingOptions(
+                      text: 'Notifications',
+                      onPressed: (){},
+
+                    ),
+                    const SizedBox(height: 2,),
+                    SettingOptions(
+                      text: 'Share App',
+                      onPressed: (){},
+
+                    ),
+
+                    const SizedBox(height: 30,),
+                    SettingOptions(
+                      text: 'Rate Us',
+                      onPressed: (){},
+
+                    ),
+                    const SizedBox(height: 2,),
+                    SettingOptions(
+                      text: 'Privacy policy',
+                      onPressed: (){},
+
+                    ),
+
+                    const SizedBox(height: 2,),
+                    SettingOptions(
+                      text: 'Terms of use',
+                      onPressed: (){},
+
+                    ),
+
+                    const SizedBox(height: 2,),
+                    SettingOptions(
+                      text: 'Contact us',
+                      onPressed: (){},
+
+                    ),
+
+
+                    const SizedBox(height: 30,),
+                    SettingOptions(
+                      text: 'Change password',
+                      onPressed: (){},
+
+                    ),
+
+
+                    const SizedBox(height: 30,),
+                    SettingOptions(
+                      text: 'Log out',
+                      dialog:
+                        CustomAlertDialog(
+                          title: 'Warning',
+                          content: 'Are you sure you want to log out?',
+                          acceptText: 'Yes',
+                          rejectText: 'No',
+                          onAccept: () {
+                            Navigator.pop(context);
+                            _authService.signOutUser();
+                            nextScreenReplace(context, LoginPage(showRegisterPage: () {  },));
+                          },
+                          onReject: () {
+                            Navigator.pop(context);
+                          }
+                        ),
+
+                      textColor: Colors.red,
+                      backgroundColor: Colors.red,
+                    ),
+
+                  ],
                 )
               ],
           ),
