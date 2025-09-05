9b9606e897e7434d469129bf961969e2f0c73639
willhitman <giftwt9wt@gmail.com>
2025-09-05 18:09:46 +0200

    adding color theme.... not yet consumed anywhere though :(

---
+++ lib/widgets/color_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ColourTheme {
+  // Primary style (your original)
+  static (Color, Color) primary() {
+    return (const Color(0xFF064151), Colors.white);
+  }
+
+  // Secondary style
+  static (Color, Color) secondary() {
+    return (Colors.grey[300]!, Colors.black87);
+  }
+
+  // Danger style
+  static (Color, Color) danger() {
+    return (Colors.red[700]!, Colors.white);
+  }
+
+  // Warning style
+  static (Color, Color) warning() {
+    return (Colors.orange[700]!, Colors.white);
+  }
+
+  // Success style
+  static (Color, Color) success() {
+    return (Colors.green[700]!, Colors.white);
+  }
+}
