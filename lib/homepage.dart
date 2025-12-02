9aff42d3f369a5a62b2ac93c6accf2ecfd106832
willhitman <giftwt9wt@gmail.com>
2025-12-02 09:35:22 +0200

    ... page name change code change

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:inkombe_flutter/pages/database_page.dart';
+import 'package:inkombe_flutter/pages/manage_cattle_page.dart';
 import 'package:inkombe_flutter/pages/home_screen/home_page.dart';
 import 'package:inkombe_flutter/pages/scan_page.dart';
 import 'package:inkombe_flutter/pages/settings_page.dart';
@@ -25,7 +25,7 @@ class _HomepageState extends State<Homepage> {
       style: optionStyle,
     ),
     ScanPage(),
-    DatabasePage(),
+    ManageCattlePage(),
     SettingsPage()
   ];
 
